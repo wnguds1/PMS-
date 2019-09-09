9d9b83fc0d9a13fc17e393b96f1f48870cae186f
JungGunSoo <48461256+JungGunSoo@users.noreply.github.com>
2019-09-09 10:36:08 +0900

    curl테스트

---
+++ JJY181205Board/src/main/java/com/jjy/board/test/Test.java
@@ -0,0 +1,25 @@
+package com.jjy.board.test;
+
+import java.io.*;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+		ProcessBuilder pb = new ProcessBuilder("curl",
+				"-s", 
+				"http://localhost:81/redmine/issues.json?key=b579df13ce6a80d247fbe91358024acfb6d91ac3");
+		System.out.println("Asdf"+pb);
+		Process p = pb.start();
+		InputStream is = p.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		StringBuilder responseStrBuilder = new StringBuilder();
+
+		String line = new String();
+
+		while ((line = br.readLine()) != null) {
+		    System.out.println("read line from curl command: " + line);
+		    responseStrBuilder.append(line);
+		}
+		
+	}
+}
